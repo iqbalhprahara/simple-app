da93086c7f1870eba031c792c01ecaf5d0db9c00
Iqbal Hikmat Prahara <iqbalhprahara@gmail.com>
2023-09-19 16:32:55 +0000

    chore: initialize swagger configuration and route

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Simple App API')
+    .setDescription('This is the documentation for Simple App API.')
+    .setVersion('0.0.1')
+    .addTag('simple-app')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig, {
+    operationIdFactory: (controllerKey: string, methodKey: string) => `${methodKey}`,
+  });
+  
+  SwaggerModule.setup('api', app, swaggerDocument);
+
   await app.listen(3000);
 }
 bootstrap();
